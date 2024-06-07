1ab906b8520306cd9e7cf946aa1a28cccda370dd
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-07 15:23:49 -0700

    Commit

---
+++ CustomeDataAPICalling/Controller/SkeletonView.swift
@@ -0,0 +1,8 @@
+//
+//  SkeletonView.swift
+//  CustomeDataAPICalling
+//
+//  Created by Arpit iOS Dev. on 07/06/24.
+//
+
+import Foundation
---
+++ CustomeDataAPICalling/View/XIB/NoDataView.swift
@@ -0,0 +1,8 @@
+//
+//  NoDataView.swift
+//  CustomeDataAPICalling
+//
+//  Created by Arpit iOS Dev. on 07/06/24.
+//
+
+import Foundation
