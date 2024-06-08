dc1a1bd2135b8908a731a729a3649814ced0238b
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-07 17:04:05 -0700

    Commit

--- CustomeDataAPICalling/Controller/APIShowDataViewController.swift
+++ CustomeDataAPICalling/Controller/APIShowDataViewController.swift
@@ -9,69 +9,114 @@ import UIKit
 import Alamofire
 import SDWebImage
 
-class APIShowDataViewController: UIViewController {
+class APIShowDataViewController: UIViewController  {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    @IBOutlet weak var noInternetView: UIView!
-    @IBOutlet weak var retryButton: UIButton!
     
+    var noInternetView: NoInternetView!
+    var noDataView: NoDataView!
     var query: String?
     var items: [Item] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.isHidden = true
-        noInternetView.isHidden = true
-        if let query = query {
-            if isConnectedToInternet() {
-                showLoaderAndFetchData(query: query)
-                // showAlert(title: "Internet", message: "Internet Connected")
-            } else {
-                showNoInternetView()
-            }
-        }
-    }
-    
-    @IBAction func retryButtonTapped(_ sender: UIButton) {
-        if isConnectedToInternet() {
-            noInternetView.isHidden = true
-            showLoaderAndFetchData(query: query!)
-        } else {
-            showAlert(title: "No Internet", message: "Please check your internet connection and try again.")
-        }
-    }
-    
-    func showLoaderAndFetchData(query: String) {
-        activityIndicator.startAnimating()
-        activityIndicator.style = .large
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.activityIndicator.stopAnimating()
-            self.tableView.isHidden = false
-            self.searchGitHubUsers(query: query)
-        }
-    }
-    
-    func searchGitHubUsers(query: String) {
-        let url = "https://api.github.com/search/users"
-        let parameters: [String: Any] = [
-            "q": query
-        ]
+         tableView.isHidden = true
         
-        AF.request(url, method: .get, parameters: parameters).responseDecodable(of: Welcome.self) { response in
-            
-            switch response.result {
-            case .success(let searchResponse):
-                self.items = searchResponse.items
-                self.tableView.reloadData()
-            case .failure(let error):
-                print("Error occurred: \(error)")
+        
+        setupNoInternetView()
+        setupNoDataView()
+        
+                if let query = query {
+                    if isConnectedToInternet() {
+                        showLoaderAndFetchData(query: query)
+                    } else {
+                        showNoInternetView()
+                    }
+                }
+            }
+    
+    
+    func setupNoInternetView() {
+        noInternetView = NoInternetView()
+        noInternetView.translatesAutoresizingMaskIntoConstraints = false
+        noInternetView.retryButton.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
+        view.addSubview(noInternetView)
+        
+        NSLayoutConstraint.activate([
+            noInternetView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            noInternetView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            noInternetView.topAnchor.constraint(equalTo: view.topAnchor),
+            noInternetView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        noInternetView.isHidden = true
+    }
+    
+    func setupNoDataView() {
+        noDataView = NoDataView()
+        noDataView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(noDataView)
+        
+        NSLayoutConstraint.activate([
+            noDataView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            noDataView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            noDataView.topAnchor.constraint(equalTo: view.topAnchor),
+            noDataView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        noDataView.isHidden = true
+    }
+    
+        @objc func retryButtonTapped() {
+            if isConnectedToInternet() {
+                noInternetView.isHidden = true
+                showLoaderAndFetchData(query: query!)
+            } else {
+                showAlert(title: "No Internet", message: "Please check your internet connection and try again.")
             }
         }
+    
+        func showLoaderAndFetchData(query: String) {
+            activityIndicator.startAnimating()
+            activityIndicator.style = .large
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                self.activityIndicator.stopAnimating()
+                self.tableView.isHidden = false
+                self.searchGitHubUsers(query: query)
+            }
+        }
+    
+        func searchGitHubUsers(query: String) {
+            let url = "https://api.github.com/search/users"
+            let parameters: [String: Any] = [
+                "q": query
+            ]
+    
+            AF.request(url, method: .get, parameters: parameters).responseDecodable(of: Welcome.self) { response in
+    
+                switch response.result {
+                case .success(let searchResponse):
+                    self.items = searchResponse.items
+                    if self.items.isEmpty {
+                        self.showNoDataView()
+                    } else {
+                        self.noDataView.isHidden = true
+                        self.tableView.isHidden = false
+                        self.tableView.reloadData()
+                    }
+                case .failure(let error):
+                    print("Error occurred: \(error)")
+                }
+            }
+        }
+    
+    func showNoDataView() {
+        noDataView.isHidden = false
+        tableView.isHidden = true
     }
     
     func showAlert(title: String, message: String) {
@@ -85,10 +130,10 @@ class APIShowDataViewController: UIViewController {
         return networkManager?.isReachable ?? false
     }
     
-    func showNoInternetView() {
-        noInternetView.isHidden = false
-        activityIndicator.stopAnimating()
-    }
+        func showNoInternetView() {
+            noInternetView.isHidden = false
+            activityIndicator.stopAnimating()
+        }
 }
 
 // MARK: - TableView Dalegate & Datasource
@@ -107,4 +152,5 @@ extension APIShowDataViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 173
     }
+    
 }
--- CustomeDataAPICalling/View/Cell/TableViewCell.swift
+++ CustomeDataAPICalling/View/Cell/TableViewCell.swift
@@ -10,16 +10,15 @@ import UIKit
 class TableViewCell: UITableViewCell {
     
     @IBOutlet weak var avtarImageView: UIImageView!
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
-
 }
+
